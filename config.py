4847421296fd7feab91148d6c88f1d83b7af7d0d
Emmanuel Temitope <emmanueltopea@gmail.com>
2021-03-10 23:14:32 +0100

    add docker configurations

--- config.py
+++ config.py
@@ -89,10 +89,24 @@ class HerokuConfig(ProductionConfig):
         app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
+class DockerConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig,
     'heroku': HerokuConfig,
+    'docker': DockerConfig
 }
